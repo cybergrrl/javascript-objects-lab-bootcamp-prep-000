a8d498c2b540e98a89b4430807240438660cb36e
scout.burghardt@gmail.com <scout.burghardt@gmail.com>
2018-05-16 11:56:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+deleteFromObjectByKey(object, key) {
+  Object.assign( {}, {delete object[key]} )
+  return object
+}
